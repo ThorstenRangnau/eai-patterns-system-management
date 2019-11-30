b75899ba7e80b67626bae52a76b8af65d9cc2e42
Thorsten Rangnau <rangnau@adference.com>
2019-11-30 20:29:16 +0100

    Add request mapping

--- receiver-application/src/main/java/com/rug/eaisystemmanagement/receiver/application/ReceiverApplicationController.java
+++ receiver-application/src/main/java/com/rug/eaisystemmanagement/receiver/application/ReceiverApplicationController.java
@@ -1,9 +1,11 @@
 package com.rug.eaisystemmanagement.receiver.application;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/message")
 public class ReceiverApplicationController {
 
     @GetMapping
